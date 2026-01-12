7380f159d1d5850e12f8a0bcbf870ad3d795c7da
zhacaixiansheng-crypto <zhacaixiansheng@gmail.com>
2026-01-12 09:48:28 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'xh-bridge-v13';
+const assets = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
